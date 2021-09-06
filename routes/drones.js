1152fc7d1d66122ef02ff98662191c420fa74fff
Joao Prates <joaoprates.az@gmail.com>
2021-09-06 14:01:26 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,56 @@
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  const drones = await Drone.find();
+  console.log(drones);
+  res.render("drones/list", {drones});
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get('/create-drone', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form');
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/create-drone', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  await Drone.create({
+    name, propellers, maxSpeed
+  });
+  res.redirect("/drones");
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:droneId', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+    const drone = await Drone.findById(req.params.droneId);
+    res.render("drones/drone-detail", drone);
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:droneId/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const drone = await Drone.findById(req.params.droneId);
+  res.render("drones/drone-edit", drone);
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:droneId/edit', async (req, res, next) => {
+  const {name, propellers, maxSpeed} = req.body;
+    await Drone.findByIdAndUpdate(req.params.droneId, {
+        name,
+        propellers,
+        maxSpeed,
+    });
+    res.redirect(`/drones/${req.params.droneId}`);
+});
+
+router.post('/drones/:droneId/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  await Drone.findByIdAndDelete(req.params.droneId);
+    res.redirect("/drones");
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,25 @@
 // Iteration #1
+require("../db");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+    {
+        name: "Anacleto",
+        propellers: 4,
+        maxSpeed: 100,
+    },
+    {
+        name: "Inacio",
+        propellers: 6,
+        maxSpeed: 150,
+    },
+    {
+        name: "Joaquim",
+        propellers: 2,
+        maxSpeed: 50,
+    }
+];
+
+Drone.insertMany(drones).then((dronesFromDB) => {
+    console.log(`Drones creates - ${dronesFromDB.length}`);
+});
